7f0f0bed879d7b8a6608551c0d3da60fbc55a78b
Guus van Meerveld <mail@guusvanmeerveld.dev>
2023-11-02 18:23:34 +0100

    flake: added flake

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+
+    # nur.url = github:nix-community/NUR;
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... } @ inputs: let
+    inherit (self) outputs;
+  in {
+    nixosConfigurations = {
+      desktop = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+
+        specialArgs = {inherit inputs outputs;};
+        modules = [
+          ./machines/desktop/configuration.nix
+        ];
+      };
+    };
+
+    homeConfigurations = {
+      "guus@desktop" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+        extraSpecialArgs = {inherit inputs outputs;};
+        modules = [./home-manager/home.nix];
+      };
+    };
+
+  };
+}
