afe12bf92b81135cd23ccbb6c5856c27f03cfa2f
Guus van Meerveld <mail@guusvanmeerveld.dev>
2023-11-05 21:37:02 +0100

    home-manager: updated gtk theme

--- home-manager/gtk.nix
+++ home-manager/gtk.nix
@@ -9,16 +9,17 @@
       package = pkgs.elementary-xfce-icon-theme;
     };
 
-    theme = {
-      name = "Zukitre-dark";
-      package = pkgs.zuki-themes;
-    };
-
     cursorTheme = {
       name = "macOS-BigSur";
       package = pkgs.apple-cursor;
       size = 2;
     };
+
+    theme =
+      {
+        name = "Fluent-Dark";
+        package = pkgs.fluent-gtk-theme;
+      };
 
     gtk3.extraConfig = {
       gtk-application-prefer-dark-theme = 1;
