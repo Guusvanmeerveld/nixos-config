02e51866832a8808b99c1736d3b162a88ce8377f
Guus van Meerveld <mail@guusvanmeerveld.dev>
2023-12-23 21:06:25 +0100

    flake: update nixos version

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
 
     nur.url = github:nix-community/NUR;
     grub2-themes.url = "github:vinceliuice/grub2-themes";
@@ -10,13 +10,13 @@
     agenix.url = "github:ryantm/agenix";
 
     nixvim = {
-      url = "github:nix-community/nixvim/nixos-23.05";
+      url = "github:nix-community/nixvim/nixos-23.11";
 
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager/release-23.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
