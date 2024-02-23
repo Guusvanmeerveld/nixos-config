c335259c364f672a596e7601774b97eb4dbd884f
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-02-23 18:51:24 +0100

    shell: added atuin history sync, added new aliases

--- home-manager/programs/zsh.nix
+++ home-manager/programs/zsh.nix
@@ -4,48 +4,59 @@ let
   p10kTheme = ./p10k.zsh;
 in
 {
-  home.packages = with pkgs; [ zoxide ];
+  home.packages = with pkgs; [ zoxide thefuck ];
 
-  programs.zsh = {
-    initExtra = ''
-      [[ ! -f ${p10kTheme} ]] || source ${p10kTheme}
-    '';
-
-    enable = true;
-
-    sessionVariables = {
-      EDITOR = "nvim";
+  programs = {
+    atuin = {
+      enable = true;
+      enableZshIntegration = true;
     };
 
-    shellAliases = {
-      hms = "home-manager switch --flake ~/nix/config";
-      nbs = "sudo nixos-rebuild switch --flake ~/nix/config";
+    zsh = {
+      initExtra = ''
+        [[ ! -f ${p10kTheme} ]] || source ${p10kTheme}
+      '';
 
-      code = "codium";
-    };
-
-    oh-my-zsh = {
       enable = true;
 
-      plugins = [ "git" "sudo" "yarn" "vscode" "colorize" ];
-    };
+      sessionVariables = {
+        EDITOR = "nvim";
+      };
 
-    zplug = {
-      enable = true;
+      shellAliases = {
+        hms = "home-manager switch --flake ~/nix/config";
+        nbs = "sudo nixos-rebuild switch --flake ~/nix/config";
+
+        down = "poweroff";
+        lsa = "ls -ah";
+        rr = "reboot";
+
+        code = "codium";
+      };
+
+      oh-my-zsh = {
+        enable = true;
+
+        plugins = [ "git" "sudo" "yarn" "vscode" "colorize" ];
+      };
+
+      zplug = {
+        enable = true;
+        plugins = [
+          { name = "zsh-users/zsh-autosuggestions"; }
+          { name = "zsh-users/zsh-syntax-highlighting"; }
+          { name = "ajeetdsouza/zoxide"; }
+          { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
+        ];
+      };
+
       plugins = [
-        { name = "zsh-users/zsh-autosuggestions"; }
-        { name = "zsh-users/zsh-syntax-highlighting"; }
-        { name = "ajeetdsouza/zoxide"; }
-        { name = "romkatv/powerlevel10k"; tags = [ as:theme depth:1 ]; }
+        {
+          name = "powerlevel10k";
+          src = pkgs.zsh-powerlevel10k;
+          file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
+        }
       ];
     };
-
-    plugins = [
-      {
-        name = "powerlevel10k";
-        src = pkgs.zsh-powerlevel10k;
-        file = "share/zsh-powerlevel10k/powerlevel10k.zsh-theme";
-      }
-    ];
   };
 }
