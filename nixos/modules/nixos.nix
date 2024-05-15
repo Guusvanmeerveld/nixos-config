cb7aa8b620beddabfa41be9b909c115696f1e0a1
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-05-15 20:56:01 +0200

    nix: automatically collect garbage every week

--- nixos/modules/nixos.nix
+++ nixos/modules/nixos.nix
@@ -1,9 +1,17 @@
 {...}: {
   config = {
-    nix.settings = {
-      experimental-features = ["nix-command" "flakes"];
-      auto-optimise-store = true;
-      warn-dirty = false;
+    nix = {
+      settings = {
+        experimental-features = ["nix-command" "flakes"];
+        auto-optimise-store = true;
+        warn-dirty = false;
+      };
+
+      gc = {
+        automatic = true;
+        options = "--delete-older-than 30d";
+        dates = "weekly";
+      };
     };
   };
 }
