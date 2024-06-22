69d4a3a9ccef49ef20ce3190fe9d8d07e2d2bec8
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-06-22 21:16:46 +0200

    moved flake registry configuration to general modules configuration

--- machines/desktop/configuration.nix
+++ machines/desktop/configuration.nix
@@ -27,19 +27,6 @@
     };
   };
 
-  # This will add each flake input as a registry
-  # To make nix3 commands consistent with your flake
-  nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-  nix.nixPath = ["/etc/nix/path"];
-  environment.etc =
-    lib.mapAttrs'
-    (name: value: {
-      name = "nix/path/${name}";
-      value.source = value.flake;
-    })
-    config.nix.registry;
-
   networking.hostName = "desktop";
 
   networking.networkmanager.enable = true;
@@ -84,7 +71,6 @@
       docker.enable = true;
 
       graphical = {
-        corsairKeyboard = true;
         steam.enable = true;
       };
 
--- nixos/modules/nixos.nix
+++ nixos/modules/nixos.nix
@@ -1,5 +1,23 @@
-{...}: {
+{
+  inputs,
+  lib,
+  config,
+  ...
+}: {
   config = {
+    # This will add each flake input as a registry
+    # To make nix3 commands consistent with your flake
+    nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+
+    nix.nixPath = ["/etc/nix/path"];
+    environment.etc =
+      lib.mapAttrs'
+      (name: value: {
+        name = "nix/path/${name}";
+        value.source = value.flake;
+      })
+      config.nix.registry;
+
     nix = {
       settings = {
         experimental-features = ["nix-command" "flakes"];
