d7148e51f016d758c2aab5266abd4a1379b041a7
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-07-12 23:22:19 +0200

    wayland: added option to enable xdg portal

--- nixos/modules/wm/wayland/default.nix
+++ nixos/modules/wm/wayland/default.nix
@@ -1,3 +1,19 @@
-{...}: {
+{
+  lib,
+  config,
+  ...
+}: let
+  cfg = config.custom.wm.wayland.sway;
+in {
   imports = [./sway.nix];
+
+  options = {
+    custom.wm.wayland = {
+      enable = lib.mkEnableOption "Enable wayland support";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    xdg.portal.wlr.enable = true;
+  };
 }
