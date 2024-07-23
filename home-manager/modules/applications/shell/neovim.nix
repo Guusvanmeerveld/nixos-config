5090e0683c46589b72c62e6217143c7a79f271bf
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-07-23 17:41:51 +0200

    nvim: cleanup neo-tree config

--- home-manager/modules/applications/shell/neovim.nix
+++ home-manager/modules/applications/shell/neovim.nix
@@ -79,13 +79,8 @@ in {
         # File tree
         neo-tree = {
           enable = true;
-          enableDiagnostics = true;
-          enableGitStatus = true;
-          enableModifiedMarkers = true;
-          enableRefreshOnWrite = true;
+          autoCleanAfterSessionRestore = true;
           closeIfLastWindow = true;
-
-          popupBorderStyle = "rounded"; # Type: null or one of “NC”, “double”, “none”, “rounded”, “shadow”, “single”, “solid” or raw lua code
 
           buffers = {
             bindToCwd = false;
