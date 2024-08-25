f21a74c02e70b7fc9358100ce293964db2fdec12
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-08-25 13:34:34 +0200

    watchtower: added option to define schedule

--- nixos/modules/applications/services/docker/watchtower/default.nix
+++ nixos/modules/applications/services/docker/watchtower/default.nix
@@ -2,12 +2,21 @@
      options = {
         custom.applications.services.docker.watchtower = {
             enable = lib.mkEnableOption "Enable watchtower docker service";
+
+            schedule = lib.mkOption {
+                type = lib.types.str;
+                default = "@daily";
+            };
         };
     };
 
     config = lib.mkIf cfg.enable {
         services.docker-compose.projects."watchtower" = {
             file = ./docker-compose.yaml;
+
+            env = {
+                SCHEDULE = cfg.schedule;
+            };
         };
     };
 } 
