29751a7ee6927e7b7b3c4f6fd561d8fad697a4f7
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-08-30 17:48:06 +0200

    docker: added module for uptime-kuma

--- nixos/modules/applications/services/docker/default.nix
+++ nixos/modules/applications/services/docker/default.nix
@@ -17,6 +17,7 @@ in {
     ./dashdot
     ./syncthing
     ./jellyfin
+    ./uptime-kuma
   ];
 
   options = {
---
+++ nixos/modules/applications/services/docker/uptime-kuma/default.nix
@@ -0,0 +1,29 @@
+{
+  lib,
+  config,
+  ...
+}: let
+  dockerConfig = config.custom.applications.services.docker;
+
+  cfg = dockerConfig.uptime-kuma;
+  networking = dockerConfig.networking;
+  storage = dockerConfig.storage;
+in {
+  options = {
+    custom.applications.services.docker.uptime-kuma = {
+      enable = lib.mkEnableOption "Enable Uptime Kuma docker service";
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.docker-compose.projects."uptime-kuma" = {
+      file = ./docker-compose.yaml;
+
+      env = {
+        INTERNAL_NETWORK_NAME = networking.internalNetworkName;
+
+        DATA_DIR = storage.storageDir + "/uptime-kuma";
+      };
+    };
+  };
+}
