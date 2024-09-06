1815e860e473ee6c66787a19a563b550a5df2e14
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-09-06 22:28:55 +0200

    orchid: added smb shares, updated dns server config

--- machines/orchid/nixos/samba.nix
+++ machines/orchid/nixos/samba.nix
@@ -1,52 +1,70 @@
 {...}: {
-    config = {
-        services.samba = {
-            enable = true;
-            openFirewall = true;
+  config = {
+    services.samba = {
+      enable = true;
+      openFirewall = true;
 
-            securityType = "user";
+      securityType = "user";
 
-            extraConfig = ''
-                workgroup = WORKGROUP
-                server string = orchid
-                netbios name = orchid
-                security = user 
+      extraConfig = ''
+        workgroup = WORKGROUP
+        server string = orchid
+        netbios name = orchid
+        security = user
 
-                # use sendfile = yes
-                # max protocol = smb2
+        # use sendfile = yes
+        # max protocol = smb2
 
-                # note: localhost is the ipv6 localhost ::1
-                hosts allow = 192.168.2. 127.0.0.1 localhost
-                hosts deny = 0.0.0.0/0
+        # note: localhost is the ipv6 localhost ::1
+        hosts allow = 192.168.2. 127.0.0.1 localhost
+        hosts deny = 0.0.0.0/0
 
-                guest account = nobody
-                map to guest = bad user
-            '';
+        guest account = nobody
+        map to guest = bad user
+      '';
 
-            shares = {
-                iso = {
-                    path = "/mnt/data/iso";
-                    browseable = "yes";
-                    "read only" = "no";
-                    "guest ok" = "no";
-                    "create mask" = "0644";
-                    "directory mask" = "0755";
-                };
-                
-                games = {
-                    path = "/mnt/data/games";
-                    browseable = "yes";
-                    "read only" = "no";
-                    "guest ok" = "no";
-                    "create mask" = "0644";
-                    "directory mask" = "0755";
-                };
-            };
+      shares = {
+        iso = {
+          path = "/mnt/data/iso";
+          browseable = "yes";
+          "read only" = "no";
+          "guest ok" = "no";
+          "create mask" = "0644";
+          "directory mask" = "0755";
         };
 
-        services.samba-wsdd = {
-            enable = true;
-            openFirewall = true;
+        games = {
+          path = "/mnt/data/games";
+          browseable = "yes";
+          "read only" = "no";
+          "guest ok" = "no";
+          "create mask" = "0644";
+          "directory mask" = "0755";
         };
+
+        media = {
+          path = "/mnt/data/media";
+          browseable = "yes";
+          "read only" = "no";
+          "guest ok" = "no";
+          "create mask" = "0644";
+          "directory mask" = "0755";
+        };
+
+        apps = {
+          path = "/mnt/data/apps";
+          browseable = "yes";
+          "read only" = "no";
+          "guest ok" = "no";
+          "create mask" = "0644";
+          "directory mask" = "0755";
+        };
+      };
     };
+
+    services.samba-wsdd = {
+      enable = true;
+      openFirewall = true;
+    };
+  };
 }
