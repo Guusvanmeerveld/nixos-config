c4e223569fb5d795f33f50cec1d0720de0c0d4bc
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-09-06 22:45:27 +0200

    nixos(wayland): removed xdg portal config

--- nixos/modules/wm/wayland/default.nix
+++ nixos/modules/wm/wayland/default.nix
@@ -1,19 +1,3 @@
-{
-  lib,
-  config,
-  ...
-}: let
-  cfg = config.custom.wm.wayland.sway;
-in {
+{...}: {
   imports = [./sway];
-
-  options = {
-    custom.wm.wayland = {
-      enable = lib.mkEnableOption "Enable wayland support";
-    };
-  };
-
-  config = lib.mkIf cfg.enable {
-    xdg.portal.wlr.enable = true;
-  };
 }
