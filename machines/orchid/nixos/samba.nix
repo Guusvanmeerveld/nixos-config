728c189108027a8b76ef3305c30b198c6f567d76
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-09-19 17:34:21 +0200

    orchid: added immich share

--- machines/orchid/nixos/samba.nix
+++ machines/orchid/nixos/samba.nix
@@ -69,6 +69,12 @@
             path = "/mnt/data/apps/syncthing";
           }
           // baseConfig;
+
+        immich =
+          {
+            path = "/mnt/data/apps/immich";
+          }
+          // baseConfig;
       };
     };
 
