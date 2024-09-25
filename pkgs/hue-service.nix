da5750e820a1be573e692778183a86a710d7deef
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-09-25 11:19:46 +0200

    hue-service: remove pkgs import

--- pkgs/hue-service.nix
+++ pkgs/hue-service.nix
@@ -1,22 +1,23 @@
 {
+  buildKodiAddon,
+  fetchFromGitHub,
+  requests,
   lib,
-  pkgs,
-  ...
 }: let
 in
-  pkgs.kodiPackages.buildKodiAddon rec {
+  buildKodiAddon rec {
     pname = "hue-service";
     namespace = "script.service.hue";
     version = "2.0.10";
 
-    src = pkgs.fetchFromGitHub {
+    src = fetchFromGitHub {
       owner = "zim514";
       repo = "script.service.hue";
       rev = "v${version}";
       sha256 = "sha256-JrCGDNvkj1NI3NqH1DqC73/aqVZnhY2fwX/3HZg4qi8=";
     };
 
-    propagatedBuildInputs = with pkgs.kodiPackages; [
+    propagatedBuildInputs = [
       requests
     ];
 
