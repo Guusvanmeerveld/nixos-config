4c206e17254f3eeda8cd61494249abedf4f7e9c3
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-10-11 19:32:42 +0200

    mpd: configure to use mpris module

--- home-manager/modules/applications/services/mpd.nix
+++ home-manager/modules/applications/services/mpd.nix
@@ -1,10 +1,13 @@
 {
+  outputs,
   lib,
   config,
   ...
 }: let
   cfg = config.custom.applications.services.mpd;
 in {
+  imports = [outputs.homeManagerModules.mpdris2];
+
   options = {
     custom.applications.services.mpd = {
       enable = lib.mkEnableOption "Enable MPD";
@@ -14,6 +17,8 @@ in {
   config = lib.mkIf cfg.enable {
     services.mpd = {
       enable = true;
+
+      mpris.enable = true;
 
       extraConfig = ''
         audio_output {
