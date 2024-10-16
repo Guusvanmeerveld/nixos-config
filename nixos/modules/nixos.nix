3712294b402660d224839d1f6499a8399fff1cdf
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-10-16 19:59:02 +0200

    nixos: remove nix path mapping

--- nixos/modules/nixos.nix
+++ nixos/modules/nixos.nix
@@ -20,16 +20,16 @@
 
     # This will add each flake input as a registry
     # To make nix3 commands consistent with your flake
-    nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
+    # nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
 
-    nix.nixPath = ["/etc/nix/path"];
-    environment.etc =
-      lib.mapAttrs'
-      (name: value: {
-        name = "nix/path/${name}";
-        value.source = value.flake;
-      })
-      config.nix.registry;
+    # nix.nixPath = ["/etc/nix/path"];
+    # environment.etc =
+    #   lib.mapAttrs'
+    #   (name: value: {
+    #     name = "nix/path/${name}";
+    #     value.source = value.flake;
+    #   })
+    #   config.nix.registry;
 
     nix = {
       settings = {
