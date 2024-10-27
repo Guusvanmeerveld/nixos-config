42d85ffc98ec81b051a83b79350091e48bfc3cd2
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-10-27 13:03:07 +0100

    nixos(sway): add option for swayfx

--- nixos/modules/wm/wayland/sway/default.nix
+++ nixos/modules/wm/wayland/sway/default.nix
@@ -5,19 +5,29 @@
   ...
 }: let
   cfg = config.custom.wm.wayland.sway;
+
+  package =
+    if cfg.useFx
+    then pkgs.swayfx
+    else pkgs.sway;
 in {
   imports = [./osd.nix];
 
   options = {
     custom.wm.wayland.sway = {
       enable = lib.mkEnableOption "Enable sway window manager";
+
+      useFx = lib.mkOption {
+        type = lib.types.bool;
+        default = true;
+      };
     };
   };
 
   config = lib.mkIf cfg.enable {
     custom.wm.default = {
       name = "sway";
-      path = "${pkgs.sway}/bin/sway";
+      path = lib.getExe package;
     };
 
     environment.sessionVariables.NIXOS_OZONE_WL = "1";
