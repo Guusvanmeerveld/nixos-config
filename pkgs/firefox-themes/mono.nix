7853bf7cb56ae590593fc3672eea742a9ffdac30
Guus van Meerveld <mail@guusvanmeerveld.dev>
2024-10-29 21:34:56 +0100

    firefox-mono-theme: fix table titles not being centered

--- pkgs/firefox-themes/mono.nix
+++ pkgs/firefox-themes/mono.nix
@@ -1,21 +1,47 @@
 {
   pkgs,
   fetchFromGitHub,
-}:
-pkgs.stdenv.mkDerivation rec {
-  name = "Mono-firefox-theme";
-  version = "0.6";
+}: let
+  fixCenterTabTitle = ''
+    .tab-icon-stack {
+      margin-inline-start: auto
+    }
 
-  src = fetchFromGitHub {
-    owner = "witalihirsch";
-    repo = name;
-    rev = "111c1c4768deb36b224febd29cc7a5517a162675";
-    hash = "sha256-0i90s/9WruvJQm3YoI4J7zsAdkJhTH0OGp5jwOaZINQ=";
-  };
+    .tab-label-container {
+      max-width: min-content;
+      margin-inline-end: auto;
+    }
 
-  buildPhase = ''
-    mkdir -p $out/local/share
+    .tab-label-container.proton {
+      display: grid !important;
+      justify-items: safe center !important;
+      text-align: center !important;
+    }
 
-    cp firefox $out/local/share -r
+    .tab-label {
+      overflow: hidden !important;
+      text-align: center !important;
+    }
   '';
-}
+in
+  pkgs.stdenv.mkDerivation rec {
+    name = "Mono-firefox-theme";
+    version = "0.6";
+
+    src = fetchFromGitHub {
+      owner = "witalihirsch";
+      repo = name;
+      rev = "111c1c4768deb36b224febd29cc7a5517a162675";
+      hash = "sha256-0i90s/9WruvJQm3YoI4J7zsAdkJhTH0OGp5jwOaZINQ=";
+    };
+
+    postPatch = ''
+      echo "${fixCenterTabTitle}" >> firefox/userChrome.css
+    '';
+
+    buildPhase = ''
+      mkdir -p $out/local/share
+
+      cp firefox $out/local/share -r
+    '';
+  }
