c89ba409e959a2afaf3d8e8701fbd156ca729a27
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-01-16 14:15:44 +0100

    wireguard: create module to configure server and client

--- nixos/applications/default.nix
+++ nixos/applications/default.nix
@@ -1,3 +1,3 @@
 {...}: {
-  imports = [./graphical ./shell ./services ./plymouth.nix ./qemu.nix ./utils.nix ./android.nix ./wireguard.nix ./waydroid.nix ./mconnect.nix];
+  imports = [./graphical ./shell ./services ./plymouth.nix ./qemu.nix ./utils.nix ./android.nix ./wireguard ./waydroid.nix ./mconnect.nix];
 }
