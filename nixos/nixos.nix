d138b293fdcf7639db364a131a24df165e22e0ab
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-02-05 16:31:21 +0100

    nixos: make nix path & registry match flake inputs

--- nixos/nixos.nix
+++ nixos/nixos.nix
@@ -1,19 +1,12 @@
-{...}: {
+{
+  lib,
+  inputs,
+  ...
+}: {
   config = {
-    # This will add each flake input as a registry
-    # To make nix3 commands consistent with your flake
-    # nix.registry = (lib.mapAttrs (_: flake: {inherit flake;})) ((lib.filterAttrs (_: lib.isType "flake")) inputs);
-
-    # nix.nixPath = ["/etc/nix/path"];
-    # environment.etc =
-    #   lib.mapAttrs'
-    #   (name: value: {
-    #     name = "nix/path/${name}";
-    #     value.source = value.flake;
-    #   })
-    #   config.nix.registry;
-
-    nix = {
+    nix = let
+      flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs;
+    in {
       settings = {
         substituters = [
           "https://guusvanmeerveld.cachix.org"
@@ -29,6 +22,11 @@
         auto-optimise-store = true;
         warn-dirty = false;
       };
+
+      # Opinionated: make flake registry and nix path match flake inputs
+      registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+      nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+      channel.enable = false;
 
       # gc = {
       #   automatic = true;
