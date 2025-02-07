aa5997c9c290ff2ba873361ac66f109a37c21aff
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-02-07 14:56:46 +0100

    tmux: add continuum and resurrect plugins

--- home-manager/programs/cli/tmux.nix
+++ home-manager/programs/cli/tmux.nix
@@ -21,6 +21,17 @@ in {
 
       plugins = with pkgs; [
         tmuxPlugins.cpu
+        {
+          plugin = tmuxPlugins.resurrect;
+          extraConfig = "set -g @resurrect-strategy-nvim 'session'";
+        }
+        {
+          plugin = tmuxPlugins.continuum;
+          extraConfig = ''
+            set -g @continuum-restore 'on'
+            set -g @continuum-save-interval '60' # minutes
+          '';
+        }
       ];
     };
   };
