3b4c6c166f81c486bebf94ddf08154b1ec98b3e6
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-02-07 17:13:55 +0100

    corsair: add auto start item to start app on wm start

--- nixos/hardware/input/corsair.nix
+++ nixos/hardware/input/corsair.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   cfg = config.custom.hardware.input.corsair;
@@ -11,5 +12,12 @@ in {
 
   config = lib.mkIf cfg.enable {
     hardware.ckb-next.enable = true;
+
+    environment.systemPackages = [
+      (pkgs.makeAutostartItem {
+        name = "ckb-next";
+        package = pkgs.ckb-next;
+      })
+    ];
   };
 }
