127fd4320aff10dbed648a7d658fa0d4b49d822f
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-02-09 21:37:40 +0100

    atuin: move back to stable

--- home-manager/programs/cli/atuin.nix
+++ home-manager/programs/cli/atuin.nix
@@ -1,14 +1,10 @@
 {
   lib,
   config,
-  pkgs,
   ...
 }: let
   cfg = config.custom.programs.cli.atuin;
-  shell = config.custom.programs.cli;
 in {
-  imports = [./zsh.nix];
-
   options = {
     custom.programs.cli.atuin = {
       enable = lib.mkEnableOption "Enable Atuin history sync";
@@ -23,8 +19,6 @@ in {
   config = lib.mkIf cfg.enable {
     programs.atuin = {
       enable = true;
-      package = pkgs.unstable.atuin;
-      enableZshIntegration = shell.zsh.enable;
 
       settings = {
         sync_address = cfg.server;
