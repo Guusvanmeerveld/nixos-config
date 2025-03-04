e9a085baf5c4b2d769fd17e68c3a36553d242c70
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-03-04 20:35:04 +0100

    phone: configure nixpath & add packages

--- machines/phone/configuration.nix
+++ machines/phone/configuration.nix
@@ -3,13 +3,14 @@
   inputs,
   outputs,
   shared,
+  lib,
   ...
 }: {
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
   environment = {
     etcBackupExtension = ".bak";
 
-    packages = with pkgs; [vim git zsh openssh btop unzip zip];
+    packages = with pkgs; [vim git zsh openssh btop unzip zip htop speedtest-cli perl procps killall diffutils findutils utillinux tzdata hostname man gnugrep gnupg gnused gnutar gawk bzip2 gzip xz];
   };
 
   time.timeZone = "Europe/Amsterdam";
@@ -18,10 +19,15 @@
     shell = "${pkgs.zsh}/bin/zsh";
   };
 
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-    warn-dirty = false
-  '';
+  nix = let flakeInputs = lib.filterAttrs (_: lib.isType "flake") inputs; in {
+    extraOptions = ''
+      experimental-features = nix-command flakes
+      warn-dirty = false
+    '';
+
+    registry = lib.mapAttrs (_: flake: {inherit flake;}) flakeInputs;
+    nixPath = lib.mapAttrsToList (n: _: "${n}=flake:${n}") flakeInputs;
+  };
 
   # Configure home-manager
   home-manager = {
