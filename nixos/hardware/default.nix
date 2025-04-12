f780146b3d87628b34a4a6a1d88b4eaa2fca4c66
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-04-12 15:21:11 +0200

    hardware: import power module

--- nixos/hardware/default.nix
+++ nixos/hardware/default.nix
@@ -1,3 +1,3 @@
 {
-  imports = [./video ./sound ./input ./bluetooth.nix ./backlight.nix ./upower.nix ./argon.nix ./openrgb.nix ./plymouth.nix ./hyperx.nix ./disko.nix];
+  imports = [./video ./sound ./input ./power ./bluetooth.nix ./backlight.nix ./upower.nix ./argon.nix ./openrgb.nix ./plymouth.nix ./hyperx.nix ./disko.nix];
 }
