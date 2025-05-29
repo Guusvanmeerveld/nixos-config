f9bdf0f6998e422326c5723ccb869b416cf64078
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-05-30 00:35:01 +0200

    tlp: set max performance on battery to `75%`

--- nixos/hardware/power/tlp.nix
+++ nixos/hardware/power/tlp.nix
@@ -23,7 +23,7 @@ in {
         CPU_MIN_PERF_ON_AC = 0;
         CPU_MAX_PERF_ON_AC = 100;
         CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 50;
+        CPU_MAX_PERF_ON_BAT = 75;
 
         #Optional helps save long term battery health
         START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
