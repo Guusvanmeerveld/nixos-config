05f4b4cc23f84f4e281ef7f6b4c55d6c7a460a2a
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-06-07 22:00:08 +0200

    tulip: switch to nixos services instead of docker containers

--- machines/tulip/configuration.nix
+++ machines/tulip/configuration.nix
@@ -1,11 +1,7 @@
 # Edit this configuration file to define what should be installed on
 # your system.  Help is available in the configuration.nix(5) man page
 # and in the NixOS manual (accessible by running ‘nixos-help’).
-{
-  config,
-  pkgs,
-  ...
-}: {
+{config, ...}: {
   imports = [
     ../../nixos
 
@@ -28,23 +24,6 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
-
-  users = {
-    users = {
-      "media" = {
-        uid = 1700;
-
-        group = "media";
-        isSystemUser = true;
-      };
-    };
-
-    groups = {
-      "media" = {
-        gid = 1700;
-      };
-    };
-  };
 
   custom = {
     users."guus" = {
@@ -79,171 +58,6 @@
       };
     };
 
-    virtualisation.docker = {
-      enable = true;
-
-      watchtower.enable = true;
-      uptime-kuma.enable = true;
-      homeassistant.enable = true;
-
-      jellyfin = {
-        enable = true;
-
-        mediaDir = "/mnt/share/media";
-
-        user.gid = config.users.groups."media".gid;
-      };
-
-      qbittorrent = {
-        enable = true;
-
-        # Make qBittorrent part of media group so it has access to media files
-        user.gid = config.users.groups."media".gid;
-
-        downloadDir = "/mnt/share/media/download";
-      };
-
-      # VPN container
-      gluetun = {
-        enable = true;
-
-        secretsFile = config.age.secrets.gluetun.path;
-      };
-
-      servarr = {
-        enable = true;
-
-        downloadDir = "/mnt/share/media/download";
-        tvDir = "/mnt/share/media/shows";
-        movieDir = "/mnt/share/media/movies";
-
-        user.gid = config.users.groups."media".gid;
-
-        bazarr.enable = true;
-        prowlarr.enable = true;
-        radarr.enable = true;
-        sonarr.enable = true;
-      };
-
-      nextcloud = {
-        enable = true;
-
-        dataDir = "/mnt/share/apps/nextcloud";
-
-        secretsFile = config.age.secrets.nextcloud.path;
-      };
-
-      ntfy = {
-        enable = true;
-
-        externalDomain = "https://ntfy.guusvanmeerveld.dev";
-      };
-
-      immich = {
-        enable = true;
-
-        secretsFile = config.age.secrets.immich.path;
-
-        uploadDir = "/mnt/share/apps/immich/upload";
-      };
-
-      unifi = {
-        enable = true;
-
-        openFirewall = true;
-
-        secretsFile = config.age.secrets.unifi.path;
-      };
-
-      caddy = let
-        modemIp = "192.168.2.254";
-
-        blockExternalVisitors = ''
-          @blocked remote_ip ${modemIp}
-          respond @blocked "Nope" 403
-        '';
-      in {
-        enable = true;
-        openFirewall = true;
-
-        caddyFile = pkgs.writeText "Caddyfile" ''
-          http://jellyfin.tlp {
-            ${blockExternalVisitors}
-
-            reverse_proxy jellyfin:8096
-          }
-
-
-
-          http://uptime.tlp {
-            ${blockExternalVisitors}
-
-            reverse_proxy uptime-kuma:3001
-          }
-
-          http://gitea.tlp {
-            ${blockExternalVisitors}
-
-            reverse_proxy gitea:3000
-          }
-
-          http://nextcloud.tlp {
-            ${blockExternalVisitors}
-
-            reverse_proxy nextcloud:80
-          }
-
-          http://homeassistant.tlp {
-            ${blockExternalVisitors}
-
-            reverse_proxy homeassistant:8123
-          }
-
-          http://immich.tlp {
-            ${blockExternalVisitors}
-
-            reverse_proxy immich-server:2283
-          }
-
-          http://portfolio.tlp {
-            ${blockExternalVisitors}
-
-            reverse_proxy portfolio:3000
-          }
-
-          http://qbittorrent.tlp {
-            ${blockExternalVisitors}
-
-            reverse_proxy gluetun:8080
-          }
-
-          http://radarr.tlp {
-            ${blockExternalVisitors}
-
-            reverse_proxy radarr:7878
-          }
-
-          http://sonarr.tlp {
-            ${blockExternalVisitors}
-
-            reverse_proxy sonarr:8989
-          }
-
-          http://prowlarr.tlp {
-            ${blockExternalVisitors}
-
-            reverse_proxy prowlarr:9696
-          }
-
-          http://bazarr.tlp {
-            ${blockExternalVisitors}
-
-            reverse_proxy bazarr:6767
-          }
-        '';
-      };
-    };
-
     services = {
       # Mount all the shares that are needed for the docker services
       samba.client = {
@@ -254,8 +68,8 @@
             host = {
               dir = "/mnt/share/media";
 
-              inherit (config.users.users.media) uid;
-              inherit (config.users.groups.media) gid;
+              inherit (config.users.users.jellyfin) uid;
+              inherit (config.users.groups.jellyfin) gid;
 
               dirMode = "0775";
               fileMode = "0664";
@@ -264,22 +78,6 @@
             remote = {
               host = "192.168.2.195";
               dir = "media";
-            };
-          }
-          {
-            host = {
-              dir = "/mnt/share/apps/nextcloud";
-
-              inherit (config.users.users.www-data) uid;
-              inherit (config.users.groups.www-data) gid;
-
-              dirMode = "0770";
-              fileMode = "0770";
-            };
-
-            remote = {
-              host = "192.168.2.195";
-              dir = "nextcloud";
             };
           }
           {
@@ -302,6 +100,45 @@
       fail2ban.enable = true;
       autoUpgrade.enable = true;
 
+      immich = {
+        enable = true;
+
+        mediaDir = "/mnt/share/apps/immich";
+        secretsFile = "/secrets/immich/secrets";
+
+        caddy.url = "http://immich.tlp";
+      };
+
+      jellyfin = {
+        enable = true;
+
+        caddy.url = "http://jellyfin.tlp";
+      };
+
+      uptime-kuma = {
+        enable = true;
+
+        caddy.url = "http://uptime.tlp";
+      };
+
+      homeassistant = {
+        enable = true;
+
+        caddy.url = "http://homeassistant.tlp";
+      };
+
+      ntfy = {
+        enable = true;
+
+        caddy.url = "http://ntfy.tlp";
+      };
+
+      unifi = {
+        enable = true;
+
+        caddy.url = "https://unifi";
+      };
+
       dnsmasq = {
         enable = true;
 
@@ -310,7 +147,6 @@
         redirects = {
           "mijnmodem.kpn" = "192.168.2.254";
 
-          ".sun" = "192.168.2.119";
           ".tlp" = "192.168.2.35";
 
           "orchid" = "192.168.2.195";
@@ -320,23 +156,6 @@
           "unifi.home" = "192.168.2.35";
         };
       };
-
-      # motd = {
-      #   enable = true;
-
-      #   settings = {
-      #     docker = {
-      #       "/caddy" = "Caddy";
-      #       "/watchtower" = "Watchtower";
-      #       "/uptime-kuma" = "Uptime Kuma";
-      #       "/syncthing" = "Syncthing";
-      #     };
-
-      #     fileSystems = {
-      #       "media" = "/mnt/share/media";
-      #     };
-      #   };
-      # };
     };
 
     programs = {
