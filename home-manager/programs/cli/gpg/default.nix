996ec3d39a0feccd330b7cd644378b8b5fdfddea
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-07-20 15:14:44 +0200

    gpg: configure ttl

--- home-manager/programs/cli/gpg/default.nix
+++ home-manager/programs/cli/gpg/default.nix
@@ -29,9 +29,13 @@ in {
 
     home.packages = [pkgs.gcr];
 
-    services.gpg-agent = {
+    services.gpg-agent = let
+      ttl = 365 * 24 * 60 * 60;
+    in {
       enable = true;
 
+      defaultCacheTtl = ttl;
+      maxCacheTtl = ttl;
       pinentry.package = pkgs.pinentry-gnome3;
     };
   };
