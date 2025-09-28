d628b1367ce3ba708192462b1e0e666a16ff0c0c
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-09-29 01:01:29 +0200

    ckb-next: fix build error by removing non essential component

--- nixos/hardware/input/corsair.nix
+++ nixos/hardware/input/corsair.nix
@@ -5,18 +5,26 @@
   ...
 }: let
   cfg = config.custom.hardware.input.corsair;
+
+  package = pkgs.ckb-next.overrideAttrs (old: {
+    cmakeFlags = (old.cmakeFlags or []) ++ ["-DUSE_DBUS_MENU=0"];
+  });
 in {
   options = {
     custom.hardware.input.corsair.enable = lib.mkEnableOption "Enable Corsair keyboard support application";
   };
 
   config = lib.mkIf cfg.enable {
-    hardware.ckb-next.enable = true;
+    hardware.ckb-next = {
+      enable = true;
+
+      inherit package;
+    };
 
     environment.systemPackages = [
       (pkgs.makeAutostartItem {
+        inherit package;
         name = "ckb-next";
-        package = pkgs.ckb-next;
       })
     ];
   };
