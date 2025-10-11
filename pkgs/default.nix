90919b7e908d117af348d553b6dc1b93e3b25376
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-10-11 16:45:25 +0200

    remove unmaintained packages

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,14 +3,7 @@
 {pkgs}: rec {
   # example = pkgs.callPackage ./example { };
 
-  mpdris2 = pkgs.callPackage ./mpdris2.nix {};
-  radb = pkgs.callPackage ./radb.nix {};
-  nwg-dock = pkgs.callPackage ./nwg-dock.nix {};
-  ryubing = pkgs.callPackage ./ryubing {};
   lidarr-plugins = pkgs.callPackage ./lidarr-plugins {};
-
-  nx_tzdb = pkgs.callPackage ./sudachi/nx_tzdb.nix {};
-  sudachi = pkgs.callPackage ./sudachi (pkgs.kdePackages // {inherit nx_tzdb;});
 
   pythonPackages = import ./python {inherit pkgs;};
 
@@ -20,7 +13,7 @@
   jellyfin = import ./jellyfin {inherit pkgs;};
 
   export = {
-    inherit mpdris2 radb ryubing lidarr-plugins;
+    inherit lidarr-plugins;
     inherit (pythonPackages) textblob;
     inherit (firefox.themes) blur mono;
     inherit (kodiPackages) hue-service;
