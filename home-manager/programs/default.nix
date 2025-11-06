23c0190094af047988f3a0ac1144cffdb4d7bc58
Guus van Meerveld <mail@guusvanmeerveld.dev>
2025-11-06 18:56:56 +0100

    home-manager(programs): set programs by default instead of force

--- home-manager/programs/default.nix
+++ home-manager/programs/default.nix
@@ -48,20 +48,20 @@ in {
 
   config = lib.mkIf cfg.default.enable {
     custom.programs = {
-      evince.enable = true;
-      librewolf.enable = true;
-      kitty.enable = true;
-      nautilus.enable = true;
-      feishin.enable = true;
-      thunderbird.enable = true;
-      mpv.enable = true;
-      loupe.enable = true;
-      file-roller.enable = true;
-      gnome-calculator.enable = true;
-      freetube.enable = true;
-      missioncenter.enable = true;
-      cheese.enable = true;
-      jellyfin-client.enable = true;
+      evince.enable = lib.mkDefault true;
+      librewolf.enable = lib.mkDefault true;
+      kitty.enable = lib.mkDefault true;
+      nautilus.enable = lib.mkDefault true;
+      feishin.enable = lib.mkDefault true;
+      thunderbird.enable = lib.mkDefault true;
+      mpv.enable = lib.mkDefault true;
+      loupe.enable = lib.mkDefault true;
+      file-roller.enable = lib.mkDefault true;
+      gnome-calculator.enable = lib.mkDefault true;
+      freetube.enable = lib.mkDefault true;
+      missioncenter.enable = lib.mkDefault true;
+      cheese.enable = lib.mkDefault true;
+      jellyfin-client.enable = lib.mkDefault true;
     };
   };
 }
