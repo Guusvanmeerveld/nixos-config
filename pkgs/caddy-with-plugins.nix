8a40a5cde8be7027a0c5170c836a02192b251b12
Guus van Meerveld <mail@guusvanmeerveld.dev>
2026-06-27 15:30:30 +0200

    update `caddy-with-plugins`

--- pkgs/caddy-with-plugins.nix
+++ pkgs/caddy-with-plugins.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}:
 pkgs.caddy.withPlugins {
   plugins = ["github.com/caddy-dns/cloudflare@v0.2.3"];
-  hash = "sha256-8E6OGxwZsjPofmfi1j8dMXTkCkIRxpzhQ/KTXYIGR0w=";
+  hash = "sha256-LEpsjwy0CYx04cg42CfG6/sFv86kHmhezUG6yGedYcA=";
 }
